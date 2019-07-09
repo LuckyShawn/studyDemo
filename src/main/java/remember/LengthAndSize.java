b93c5a6a4c93c9921136450c924c0217ef4016f4
luckyshawn <luckysahwn@qq.com>
2019-07-09 18:02:00 +0800

    数组，字符串，集合

--- src/main/java/remember/LengthAndSize.java
+++ src/main/java/remember/LengthAndSize.java
@@ -6,7 +6,7 @@ import java.util.List;
 /**
  * 容易混淆的点
  * java 中的 length 属性是针对数组说的,比如说你声明了一个数组,想知道这个数组的长度则用到了 length 这个属性.
- * java 中的 length() 方法是针对字符串说的,如果想看这个字符串的长度则用到 length() 这个方法.
+ * java 中的 length() 方法是针对字符串说的,如果想看这个字符串的长度则用到 length() 这个方法.!
  * java 中的 size() 方法是针对泛型集合说的,如果想看这个泛型有多少个元素,就调用此方法来查看!
  */
 public class LengthAndSize {
