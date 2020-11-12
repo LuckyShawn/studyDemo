11c06d13f1afb9a47aa47c67a644b8c84524d689
luckyshawn <luckyshawn@qq.com>
2020-11-12 17:35:21 +0800

    leetcode合并链表排序 leetcode根据二进制排序

---
+++ src/main/java/algorithm/leetcode/MergeTwoLists.java
@@ -0,0 +1,96 @@
+package algorithm.leetcode;
+
+/**
+ * @author Shawn
+ * @title: MergeTwoLists
+ * @projectName studyDemo
+ * @description: 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * @date 2020/11/9
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class MergeTwoLists {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode l11 = new ListNode(5);
+        ListNode l111= new ListNode(9);
+        l1.next = l11;
+        l11.next = l111;
+
+
+        ListNode l2 = new ListNode(2);
+        ListNode l22 = new ListNode(3);
+        ListNode l222 = new ListNode(10);
+        l2.next = l22;
+        l22.next = l222;
+
+        ListNode temp = l1;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+
+        temp = l2;
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+
+        temp = mergeTwoLists(l1,l2);
+
+        while(temp != null){
+            System.out.println(temp.val);
+            temp = temp.next;
+        }
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/main/java/algorithm/leetcode/SortByBits.java
@@ -0,0 +1,63 @@
+package algorithm.leetcode;
+
+import java.util.*;
+
+/**
+ * @author Shawn
+ * @title: SortByBits
+ * @projectName studyDemo
+ * @description: TODO
+ * @date 2020/11/6
+ */
+public class SortByBits {
+    public static void main(String[] args) {
+        //System.out.println(Arrays.toString(sortByBits(new int[]{1,8,9,4,5,6,7,8,9})));
+
+
+        System.out.println(Arrays.toString(sortByBits2(new int[]{1,8,9,4,5,6,7,8,9})));
+    }
+
+    public static int[] sortByBits(int[] arr) {
+        int[] bit = new int[10001];
+        List<Integer> list = new ArrayList<Integer>();
+        for (int x : arr) {
+            list.add(x);
+            bit[x] = get(x);
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            public int compare(Integer x, Integer y) {
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
+                } else {
+                    return x - y;
+                }
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+
+    public static int get(int x) {
+        int res = 0;
+        while (x != 0) {
+            res += x % 2;
+            x /= 2;
+        }
+        return res;
+    }
+
+    public static int[] sortByBits2(int[] arr) {
+        int[] intArrays = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            intArrays[i] = Integer.bitCount(arr[i]) * 10000000 + arr[i];
+        }
+        Arrays.sort(intArrays);
+        for (int i = 0; i < intArrays.length; i++) {
+            intArrays[i] = intArrays[i] % 10000000;
+        }
+        return intArrays;
+    }
+
+}
