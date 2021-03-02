d84cebe672bbb2ea088a59470560ef66ba5c06bd
luckyshawn <LuckyShawn@qq.com>
2021-03-02 22:41:15 +0800

    304. 二维区域和检索 - 矩阵不可变（前缀和）

---
+++ src/main/java/algorithm/leetcode/arrays/NumMatrix.java
@@ -0,0 +1,67 @@
+package algorithm.leetcode.arrays;
+
+/**
+ * 304. 二维区域和检索 - 矩阵不可变（前缀和）
+ *
+ * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，
+ * 右下角为 (row2, col2) 。
+ *
+ *
+ * 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定 matrix = [
+ *   [3, 0, 1, 4, 2],
+ *   [5, 6, 3, 2, 1],
+ *   [1, 2, 0, 1, 5],
+ *   [4, 1, 0, 1, 7],
+ *   [1, 0, 3, 0, 5]
+ * ]
+ *
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ *  
+ *
+ * 提示：
+ *
+ * 你可以假设矩阵不可变。
+ * 会多次调用 sumRegion 方法。
+ * 你可以假设 row1 ≤ row2 且 col1 ≤ col2 。
+ * 通过次数42,282提交次数79,854
+ *
+ * 来源：力扣（LeetCode）
+ */
+public class NumMatrix {
+    public static void main(String[] args) {
+        NumMatrix numMatrix = new NumMatrix(new int[][]{{3, 0, 1, 4, 2},{5, 6, 3, 2, 1},{1, 2, 0, 1, 5},{4, 1, 0, 1, 7},{1, 0, 3, 0, 5}});
+        System.out.println(numMatrix.sumRegion(2, 1, 4, 3));
+        System.out.println(numMatrix.sumRegion(1, 1, 2, 2));
+        System.out.println(numMatrix.sumRegion(1, 2, 2, 4));
+    }
+
+    int[] [] sums;
+    public NumMatrix(int[][] matrix) {
+        int length = matrix.length;
+        if(length > 0){
+            int n = matrix[0].length;
+            sums = new int[length][n + 1];
+            for (int i = 0; i < length; i++) {
+                for (int j = 0; j < n; j++) {
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j]; //统计前缀值
+                }
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            sum += sums[i][col2 + 1] - sums[i][col1];//相加每一行的前缀值
+        }
+        return sum;
+    }
+}
