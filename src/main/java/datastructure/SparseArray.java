8f6919e7d56a19b6328082b267a237493181c8da
luckyshawn <LuckyShawn@qq.com>
2021-03-08 23:13:49 +0800

    home test

--- src/main/java/datastructure/SparseArray.java
+++ src/main/java/datastructure/SparseArray.java
@@ -6,6 +6,14 @@ import java.util.Map;
 
 /**
  * 稀疏数组
+ * * github超超时问题：
+ *  * 主要原因是因为使用了proxy代理，需要关闭代理。
+ *  *
+ *  *   git config --global http.proxy  //查看代理
+ *  *
+ *  *  结果为：localhost:1080
+ *  *
+ *  *   git config --global --unset http.proxy  //不设置代理
  */
 public class SparseArray {
 
