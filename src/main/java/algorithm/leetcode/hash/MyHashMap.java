b6282c98c5dc7ec66ff324f1d3105e43b2b8047e
luckyshawn <LuckyShawn@qq.com>
2021-03-14 20:34:31 +0800

    * 706. 设计哈希映射  * 不使用任何内建的哈希表库设计一个哈希映射（HashMap）。

---
+++ src/main/java/algorithm/leetcode/hash/MyHashMap.java
@@ -0,0 +1,144 @@
+package algorithm.leetcode.hash;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * 706. 设计哈希映射
+ * 不使用任何内建的哈希表库设计一个哈希映射（HashMap）。
+ *
+ * 实现 MyHashMap 类：
+ *
+ * MyHashMap() 用空映射初始化对象
+ * void put(int key, int value) 向 HashMap 插入一个键值对 (key, value) 。如果 key 已经存在于映射中，则更新其对应的值 value 。
+ * int get(int key) 返回特定的 key 所映射的 value ；如果映射中不包含 key 的映射，返回 -1 。
+ * void remove(key) 如果映射中存在 key 的映射，则移除 key 和它所对应的 value 。
+ *  
+ *
+ * 示例：
+ *
+ * 输入：
+ * ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+ * [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+ * 输出：
+ * [null, null, null, 1, -1, null, 1, null, -1]
+ *
+ * 解释：
+ * MyHashMap myHashMap = new MyHashMap();
+ * myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
+ * myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+ * myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+ * myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+ * myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+ * myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= key, value <= 106
+ * 最多调用 104 次 put、get 和 remove 方法
+ *  
+ *
+ * 进阶：你能否不使用内置的 HashMap 库解决此问题？
+ *
+ * 通过次数29,760提交次数48,086
+ *
+ * 来源：力扣（LeetCode）
+ *
+ * 执行用时：26 ms, 在所有 Java 提交中击败了61.90%的用户
+ * 内存消耗：43.9 MB, 在所有 Java 提交中击败了44.35%的用户
+ */
+public class MyHashMap {
+    public static void main(String[] args) {
+        MyHashMap map = new MyHashMap();
+        map.put(1,10);
+        map.put(2,20);
+        map.put(3,30);
+        map.get(1);
+        System.out.println(map.get(1));
+        map.remove(3);
+        System.out.println(map.get(3));
+    }
+
+    private LinkedList[] data;
+    public static final int BASE = 769;
+
+    /** Initialize your data structure here. */
+    public MyHashMap() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE ; i++) {
+            data[i] = new LinkedList<MyNode>();
+        }
+    }
+
+    /** value will always be non-negative. */
+    public void put(int key, int value) {
+        int hashVal = hash(key);
+        Iterator<MyNode> it = data[hashVal].iterator();
+        while(it.hasNext()){
+            MyNode element = it.next();
+            if(element.getKey() == key){
+                element.setValue(value);
+                return;
+            }
+        }
+        data[hashVal].offerLast(new MyNode(key,value));
+    }
+
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    public int get(int key) {
+        int hashVal = hash(key);
+        Iterator<MyNode> it = data[hashVal].iterator();
+        while(it.hasNext()){
+            MyNode element = it.next();
+            if(element.getKey() == key){
+                return element.getValue();
+            }
+        }
+        return -1;
+    }
+
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    public void remove(int key) {
+        int hashVal = hash(key);
+        Iterator<MyNode> it = data[hashVal].iterator();
+        while(it.hasNext()){
+            MyNode myNode = it.next();
+            if(myNode.getKey() == key){
+                data[hashVal].remove(myNode);
+                return;
+            }
+        }
+
+    }
+
+    public int hash(int key){
+        return key % BASE;
+    }
+
+    class MyNode{
+        private int key;
+        private int value;
+
+        public MyNode(int key,int value){
+            this.key = key;
+            this.value = value;
+        }
+
+        public int getKey() {
+            return key;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+    }
+
+}
