3b6167c9fa848c3732cc1a4e34dfa570318d3091
luckyshawn <LuckyShawn@qq.com>
2021-03-15 23:05:03 +0800

    按层模拟

--- src/main/java/algorithm/leetcode/arrays/SpiralOrder.java
+++ src/main/java/algorithm/leetcode/arrays/SpiralOrder.java
@@ -43,6 +43,7 @@ public class SpiralOrder {
     public static void main(String[] args) {
        int[][] matrix =  new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         System.out.println(spiralOrder(matrix));
+        System.out.println(spiralOrder01(matrix));
     }
 
     public static List<Integer> spiralOrder(int[][] matrix) {
@@ -68,6 +69,47 @@ public class SpiralOrder {
             }
             row += directions[directionIndex][0];
             column += directions[directionIndex][1];
+        }
+        return order;
+    }
+
+    /**
+     * 按层模拟
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：36.6 MB, 在所有 Java 提交中击败了54.18%的用户
+     * @param matrix
+     * @return
+     */
+    public static List<Integer> spiralOrder01(int[][] matrix) {
+        List<Integer> order = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int left = 0,right = cols - 1,top = 0,bottom = rows - 1;
+        while(left <= right && top <= bottom){
+            for (int col  = left; col <= right; col++) {
+                order.add(matrix[top][col]);
+            }
+
+            for (int row = top + 1;row <= bottom; row++){
+                order.add(matrix[row][right]);
+            }
+
+            if(left < right && top < bottom){
+                for (int col = right - 1;col > left ; col-- ){
+                    order.add(matrix[bottom][col]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    order.add(matrix[row][left]);
+                }
+            }
+            right--;
+            top++;
+            bottom--;
+            left++;
+
         }
         return order;
     }
