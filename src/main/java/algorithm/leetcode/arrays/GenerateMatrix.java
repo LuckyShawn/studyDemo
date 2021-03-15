6704978d496b19a9e2555e32aee3ede834d37a3e
luckyshawn <LuckyShawn@qq.com>
2021-03-15 23:06:43 +0800

    GenerateMatrix 59. 螺旋矩阵 II (中等)

---
+++ src/main/java/algorithm/leetcode/arrays/GenerateMatrix.java
@@ -0,0 +1,38 @@
+package algorithm.leetcode.arrays;
+
+/**
+ * @author Shawn
+ * @title: GenerateMatrix 59. 螺旋矩阵 II (中等)
+ * @projectName studyDemo
+ * @description: 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：[[1]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 20
+ * 通过次数65,396提交次数83,347
+ *
+ * 来源：力扣（LeetCode）
+ * @date 2021/3/15 0015下午 11:05
+ */
+public class GenerateMatrix {
+
+
+    public int[][] generateMatrix(int n) {
+
+        return null;
+    }
+
+}
