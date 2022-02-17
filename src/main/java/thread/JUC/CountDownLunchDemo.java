19b4a720b1da0ee1ababdd1f656dca27ee9b0548
luckyshawn <luckyshawn@qq.com>
2022-02-17 13:54:03 +0800

    JUC

--- src/main/java/thread/JUC/CountDownLunchDemo.java
+++ src/main/java/thread/JUC/CountDownLunchDemo.java
@@ -1,8 +1,9 @@
-package thread;
+package thread.JUC;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Shawn
@@ -18,11 +19,11 @@ public class CountDownLunchDemo {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(10);
         final CountDownLatch cdl = new CountDownLatch(threadCount);
-        for (int i = 0; i < threadCount; i++) {
+        for (int i = 0; i < 6; i++) {
             final int temp = i;
            threadPool.execute(() -> {
                try{
-                   Thread.sleep(1000);
+                   Thread.sleep(1000 * temp);
                    System.out.println("work:" + temp);
                } catch (InterruptedException e) {
                    e.printStackTrace();
@@ -31,8 +32,14 @@ public class CountDownLunchDemo {
                }
             });
             }
-        cdl.await();
-        threadPool.shutdown();
+        try {
+            //如果3秒过了其他线程还没有执行完，则直接往后执行
+            cdl.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw e;
+        }finally{
+            threadPool.shutdown();
+        }
         System.out.println("finish");
         }
 }
