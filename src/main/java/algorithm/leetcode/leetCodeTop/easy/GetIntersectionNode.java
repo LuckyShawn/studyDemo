dea5e10b634a512325971d47e5baa462c06f416a
luckyshawn <luckyshawn@qq.com>
2022-02-22 09:25:59 +0800

    GetIntersectionNode 160. 相交链表

---
+++ src/main/java/algorithm/leetcode/leetCodeTop/easy/GetIntersectionNode.java
@@ -0,0 +1,94 @@
+package algorithm.leetcode.leetCodeTop.easy;
+
+import algorithm.leetcode.list.ListNode;
+
+/**
+ * @author Shawn  * 来源：力扣（LeetCode）
+ * @title: GetIntersectionNode 160. 相交链表
+ * @projectName studyDemo
+ * @description: 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ *
+ * 图示两个链表在节点 c1 开始相交：
+ *
+ *
+ *
+ * 题目数据 保证 整个链式结构中不存在环。
+ *
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ *
+ * 自定义评测：
+ *
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ *
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ *
+ * 输入：intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+ * 输出：Intersected at '2'
+ * 解释：相交节点的值为 2 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [1,9,1,2,4]，链表 B 为 [3,2,4]。
+ * 在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+ *
+ * 输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+ * 输出：null
+ * 解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。
+ * 由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+ * 这两个链表不相交，因此返回 null 。
+ *
+ * @date 2022/2/18
+ */
+public class GetIntersectionNode {
+    public static void main(String[] args) {
+
+    }
+
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode aHead = headA;
+        ListNode bHead = headB;
+        while(aHead.next != null && headB.next !=null){
+
+            if(aHead == bHead){
+               return aHead;
+            }
+            aHead = aHead.next;
+            bHead = bHead.next;
+        }
+        return null;
+    }
+
+    /**
+     * 指针 pA 指向 A 链表，指针 pB 指向 B 链表，依次往后遍历
+     * 如果 pA 到了末尾，则 pA = headB 继续遍历
+     * 如果 pB 到了末尾，则 pB = headA 继续遍历
+     * 比较长的链表指针指向较短链表head时，长度差就消除了
+     * 如此，只需要将最短链表遍历两次即可找到位置
+     *
+     * 可以理解成两个人速度一致， 走过的路程一致。那么肯定会同一个时间点到达终点。如果到达终点的最后一段路两人都走的话，那么这段路上俩人肯定是肩并肩手牵手的。
+     *
+     * 走到尽头见不到你，于是走过你来时的路，等到相遇时才发现，你也走过我来时的路。
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+
+}
